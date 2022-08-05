1456688526cfd1e0da5af737a57a2d52ec0d9713
sjh9391985 <sjh9391985@gmail.com>
2022-08-05 15:01:07 +0900

    interface readonly

--- typescript/typescript-test/basic_learning/interface/interface.ts
+++ typescript/typescript-test/basic_learning/interface/interface.ts
@@ -38,8 +38,47 @@ interface IUser12 {
   isAdult?: boolean;
 }
 
-let uset3: IUser12 = {
+let user3: IUser12 = {
   name: "Son",
   age: 27,
   //isAdult 는 선택
 };
+
+/**
+ *
+ * 읽기 전용 속성
+ * 모든 속성이 readonly 일 경우, 유틸리티나 단언 타입을 활용 가능
+ */
+
+interface IUser13 {
+  readonly name: string;
+  readonly age: number;
+}
+
+let user4: IUser13 = {
+  name: "Neo",
+  age: 36,
+};
+
+// user4.name = "Son"; => readonly Error
+// user4.age = 27;
+
+interface IUser14 {
+  name: string;
+  age: number;
+}
+
+let user5: Readonly<IUser14> = {
+  name: "Neo",
+  age: 36,
+};
+// user4.name = "Son"; => readonly Error
+// user4.age = 27;
+
+let user6 = {
+  name: "Neo",
+  age: 36,
+} as const;
+
+// user.name = 'Son' => Error, Type assertion
+// user.age = 27
